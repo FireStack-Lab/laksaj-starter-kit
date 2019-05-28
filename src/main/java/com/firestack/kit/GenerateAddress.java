ffdd4fa7bbab4a9493eae58b65204cf3b9c9ed3d
xiaohuo <xiaohuo200@gmail.com>
2019-05-28 17:42:58 +0800

    chore: rewrite genetate address

--- src/main/java/com/firestack/kit/GenerateAddress.java
+++ src/main/java/com/firestack/kit/GenerateAddress.java
@@ -1,9 +1,7 @@
 package com.firestack.kit;
 
 import com.firestack.laksaj.crypto.KeyTools;
-import org.web3j.crypto.ECKeyPair;
 
-import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
@@ -11,35 +9,22 @@ import java.security.NoSuchProviderException;
 public class GenerateAddress {
     //How to generate large amount of addresses
     public static void main(String[] args) throws InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException {
-
-        while (true) {
-            String privateKey = generate();
-            if (privateKey.equals("") || privateKey.length() != 64) {
-                continue;
+        int i = 0;
+        while (i < 1000000) {
+            i++;
+            String privateKey = KeyTools.generatePrivateKey();
+            if (privateKey.length() != 64) {
+                throw new RuntimeException("generate private key error");
             }
             System.out.println("private key = " + privateKey);
+            String publicKey = KeyTools.getPublicKeyFromPrivateKey(privateKey, true);
+            if (publicKey.length() != 66) {
+                throw new RuntimeException("generate public key error");
+            }
+            System.out.println("public key = " + publicKey);
+            String address = KeyTools.getAddressFromPublicKey(publicKey);
+            System.out.println("address = " + address);
+
         }
     }
-
-    static String generate() throws InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException {
-        ECKeyPair keyPair = KeyTools.generateKeyPair();
-        BigInteger privateInteger = keyPair.getPrivateKey();
-        if (privateInteger.toString(16).length() != 64) {
-            System.out.println("wrong private key");
-            return "";
-        }
-        return privateInteger.toString(16);
-    }
-
-
-    static String padding(String privateKey) {
-        int padding = 64 - privateKey.length();
-        StringBuilder privateKeyBuilder = new StringBuilder(privateKey);
-        for (int i = 0; i < padding; i++) {
-            privateKeyBuilder.insert(0, "0");
-        }
-        return privateKeyBuilder.toString();
-    }
-
-
 }
