138ff70b20972f7bd823d7b4e7c3c03f238b498f
xiaohuo <xiaohuo200@gmail.com>
2019-05-28 16:10:15 +0800

    chore: update generate address

--- src/main/java/com/firestack/kit/GenerateAddress.java
+++ src/main/java/com/firestack/kit/GenerateAddress.java
@@ -1,7 +1,6 @@
 package com.firestack.kit;
 
 import com.firestack.laksaj.crypto.KeyTools;
-import com.firestack.laksaj.utils.ByteUtil;
 import org.web3j.crypto.ECKeyPair;
 
 import java.math.BigInteger;
@@ -12,33 +11,28 @@ import java.security.NoSuchProviderException;
 public class GenerateAddress {
     //How to generate large amount of addresses
     public static void main(String[] args) throws InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException {
-        int n = 0;
-        while (n < 100) {
-            System.out.println("--------------------------");
-            ECKeyPair keyPair = KeyTools.generateKeyPair();
-            BigInteger privateInteger = keyPair.getPrivateKey();
-            BigInteger publicInteger = keyPair.getPublicKey();
 
-            if (privateInteger.toString(16).length() > 64) {
+        while (true) {
+            String privateKey = generate();
+            if (privateKey.equals("") || privateKey.length() != 64) {
                 continue;
             }
-
-            if (privateInteger.toString(16).length() < 64) {
-                System.out.println("need pad");
-                String newPrivate = padding(privateInteger.toString(16));
-                System.out.println("private key is: " + newPrivate);
-                System.out.println("public key is: " + KeyTools.getPublicKeyFromPrivateKey(newPrivate,true).toLowerCase());
-                System.out.println("address is: " + KeyTools.getAddressFromPrivateKey(newPrivate).toLowerCase());
-                continue;
-            }
-
-            System.out.println("private key is: " + privateInteger.toString(16));
-            System.out.println("public key is: " + KeyTools.getPublicKeyFromPrivateKey(privateInteger.toString(16),true).toLowerCase());
-            System.out.println("address is: " + KeyTools.getAddressFromPrivateKey(privateInteger.toString(16)).toLowerCase());
+            System.out.println("private key = " + privateKey);
         }
     }
 
-    public static String padding(String privateKey) {
+    static String generate() throws InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException {
+        ECKeyPair keyPair = KeyTools.generateKeyPair();
+        BigInteger privateInteger = keyPair.getPrivateKey();
+        if (privateInteger.toString(16).length() != 64) {
+            System.out.println("wrong private key");
+            return "";
+        }
+        return privateInteger.toString(16);
+    }
+
+
+    static String padding(String privateKey) {
         int padding = 64 - privateKey.length();
         StringBuilder privateKeyBuilder = new StringBuilder(privateKey);
         for (int i = 0; i < padding; i++) {
